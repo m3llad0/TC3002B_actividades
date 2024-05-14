62f61917393bcdab747c584787a85ca2035aac23
m3llad0 <a01655451@tec.mx>
2024-05-14 17:58:41 -0600

    first commit

---
+++ compiladores/delta/semantics.py
@@ -0,0 +1,22 @@
+from arpeggio import PTNodeVisitor
+
+
+class SemanticMistake(Exception):
+
+    def __init__(self, message):
+        super().__init__(f'Semantic error: {message}')
+
+
+class SemanticVisitor(PTNodeVisitor):
+
+    def __init__(self, parser, **kwargs):
+        super().__init__(**kwargs)
+        self.__parser = parser
+        self.__symbol_table = []
+
+    def position(self, node):
+        return self.__parser.pos_to_linecol(node.position)
+
+    @property
+    def symbol_table(self):
+        return self.__symbol_table
